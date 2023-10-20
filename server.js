5a9f9014699f7392fa3c6a9932bdc6415b72ace9
rodriguesra <rodriguesra@hotmail.com>
2023-10-20 12:54:04 -0300

    Ajuste de porta do server.js

---
+++ server.js
@@ -0,0 +1,35 @@
+import { fastify } from "fastify";
+import { DatabasePostgres } from "./database-postgres.js";
+
+const server = fastify();
+const database = new DatabasePostgres();
+
+server.get("/videos", async (request, reply) => {
+  const search = request.query.search;
+  return await database.list(search);
+});
+
+server.post("/videos", async (request, reply) => {
+  const video = request.body;
+  await database.create(video);
+  return reply.status(201).send();
+});
+
+server.put("/videos/:id", async (request, reply) => {
+  const id = request.params.id;
+  const video = request.body;
+  await database.update(id, video);
+  return reply.status(204).send();
+});
+
+server.delete("/videos/:id", async (request, reply) => {
+  const id = request.params.id;
+  await database.delete(id);
+  return reply.status(204).send();
+});
+
+server
+  .listen({
+    port: process.env.PORT || 3333,
+  })
+  .then((r) => console.log("server listening on", r));
